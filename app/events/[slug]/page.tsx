1285effa6f3323912d3c6a56e21896c8bc416969
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2026-01-16 11:46:03 +0000

    server actions

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,9 +1,10 @@
 import BookEvent from '@/components/BookEvent'
+import EventCard from '@/components/EventCard'
+import { IEvent } from '@/database'
+import { getSimilarEventsBySlug } from '@/lib/actions/event.actions'
 import { notFound } from 'next/navigation'
 
 const BASE_URl = process.env.NEXT_PUBLIC_BASE_URL
-
-const bookings = 10
 
 const EventDetailItem = ({
   icon,
@@ -66,6 +67,12 @@ const EventDetailsPage = async ({
 
   if (!description) return notFound()
 
+  const bookings = 10
+
+  const similarEvents: IEvent[] = await getSimilarEventsBySlug(slug)
+
+  console.log(similarEvents)
+
   return (
     <section id='event'>
       <div className='header'>
@@ -105,14 +112,14 @@ const EventDetailsPage = async ({
               label={audience}
             />
           </section>
-          <EventAgenda agendaItems={JSON.parse(agenda[0])} />
+          <EventAgenda agendaItems={agenda} />
 
           <section className='flex-col-gap-2'>
             <h2>About the organizer</h2>
             <p>{organizer}</p>
           </section>
 
-          <EventTags tags={JSON.parse(tags[0])} />
+          <EventTags tags={tags} />
         </div>
 
         {/* right side - Booking form */}
@@ -130,6 +137,15 @@ const EventDetailsPage = async ({
             <BookEvent />
           </div>
         </aside>
+      </div>
+      <div className='flex flex-col w-full gap-4 pt-20'>
+        <h2>Similar Events</h2>
+        <div className='events'>
+          {similarEvents.length > 0 &&
+            similarEvents.map((similarEvent: IEvent) => (
+              <EventCard key={similarEvent.title} {...similarEvent} />
+            ))}
+        </div>
       </div>
     </section>
   )
